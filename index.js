aac19a6d7be765d532f560c37a42054a5386a1b9
Vicenzoo <108063917+plugselect01@users.noreply.github.com>
2022-06-26 18:56:45 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,68 @@
+const fetch = require('node-fetch');
+const moment = require('moment');
+const chalk = require('chalk');
+const rs = require('readline-sync');
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+const GoStumble = (auth) => new Promise((resolve, reject) => {
+
+  fetch('http://kitkabackend.eastus.cloudapp.azure.com:5010/round/finishv2/3', {
+    method: 'GET',
+    headers: {
+      'authorization': auth
+    }
+  })
+    .then(res => res.text())
+    .then(data => {
+      resolve(data);
+    })
+    .catch(err => {
+      reject(err);
+    });
+
+});
+
+(async () => {
+
+  console.log(`
+───────────▄▄▄▄▄▄▄▄▄───────────
+░██████╗░█████╗░███████╗███████╗
+██╔════╝██╔══██╗██╔════╝██╔════╝
+╚█████╗░███████║█████╗░░█████╗░░
+░╚═══██╗██╔══██║██╔══╝░░██╔══╝░░
+██████╔╝██║░░██║██║░░░░░███████╗
+╚═════╝░╚═╝░░╚═╝╚═╝░░░░░╚══════╝
+By : ${chalk.purple('Vicenzo')} - ${chalk.blue('HectorV2')}
+`);
+
+  const auth = rs.question('[+] Enter Authentication Code! : ');
+  console.log('');
+
+  while (true) {
+
+
+    const result = await GoStumble(auth);
+    if (!result) {
+
+    } else if (result.includes('User')) {
+
+      const data = JSON.parse(result);
+      const username = data.User.Username;
+      const country = data.User.Country;
+      const trophy = data.User.SkillRating;
+      const crown = data.User.Crowns;
+
+console.log(chalk.bgBlack(`\r[ ${moment().format('HH:mm:ss')} ] ${chalk.white(`User : ${username}`)} | ${chalk.yellow(`Trophy : ${trophy}`)} | ${chalk.red(`Crown : ${crown}`)}`));
+      await sleep(2600);
+
+    } else if (result == 'BANNED') {
+      console.log(chalk.bgRed(`Your Account has been Banned`));
+     break;
+    }
+  }
+
+
+})();
